8176c1ed33b50eeeaad7e442043ada80215333e0
Matthew Wideman <mvideman@s-terra.ru>
2025-05-19 11:32:43 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import ArticleList from "./components/ArticleList";
 import ArticleDetail from "./components/ArticleDetail";
