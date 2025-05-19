e82be6aa6277a6255fb83f1bcf1f8478265dbb45
Matthew Wideman <mvideman@s-terra.ru>
2025-05-19 11:38:38 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import ArticleList from "./components/ArticleList";
 import ArticleDetail from "./components/ArticleDetail";
@@ -7,16 +7,16 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <ArticleProvider>
-      <Router basename="/space-news-react">
-        <div className="container">
-          <Header />
-          <Routes>
-            <Route path="/" element={<ArticleList />} />
-            <Route path="/article/:id" element={<ArticleDetail />} />
-          </Routes>
-        </div>
-      </Router>
-    </ArticleProvider>
+      <ArticleProvider>
+        <Router>
+          <div className="container">
+            <Header />
+            <Routes>
+              <Route path="/" element={<ArticleList />} />
+              <Route path="/article/:id" element={<ArticleDetail />} />
+            </Routes>
+          </div>
+        </Router>
+      </ArticleProvider>
   );
 }
